4fc5cd5dcdd54502cb786a29ed8a198c4d454085
kaedei <kaedei@qq.com>
2024-04-09 00:25:19 +0800

    update to Angular 16

--- angular/dandanplay-libraryindex/src/app/core/helpers/AuthGuard.ts
+++ angular/dandanplay-libraryindex/src/app/core/helpers/AuthGuard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
 
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
     constructor(
         private router: Router,
         private authenticationService: AuthenticationService
